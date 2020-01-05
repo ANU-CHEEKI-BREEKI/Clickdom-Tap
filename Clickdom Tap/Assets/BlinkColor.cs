a9f4503596a537889675861468d08d4d43925967
ANU <titan.x7@gmail.com>
2020-01-05 11:16:05 +0200

    start game "cinematic" in progress...

---
+++ Clickdom Tap/Assets/BlinkColor.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class BlinkColor : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer[] sprites;
+    [SerializeField] private Gradient randomColor = new Gradient()
+    {
+        alphaKeys = new GradientAlphaKey[] { new GradientAlphaKey(1, 0) },
+        colorKeys = new GradientColorKey[] { new GradientColorKey(Color.white, 0) }
+    };
+
+    private void Reset()
+    {
+        ResetSprites();
+    }
+
+    [ContextMenu("ResetSprites")]
+    private void ResetSprites()
+    {
+        var sprites = new LinkedList<SpriteRenderer>();
+
+        var stack = new Stack<Transform>();
+        stack.Push(transform);
+        while (stack.Any())
+        {
+            var transform = stack.Pop();
+            var upe = transform.GetComponent<SpriteRenderer>();
+            if (upe != null)
+                sprites.AddLast(upe);
+
+            var childCount = transform.childCount;
+            for (int i = childCount - 1; i >= 0; i--)
+                stack.Push(transform.GetChild(i));
+        }
+        this.sprites = sprites.ToArray();
+    }
+
+    public void Blink(float t)
+    {
+        foreach (var sprite in sprites)
+            sprite.color = randomColor.Evaluate(t);
+    }
+}
---
+++ Clickdom Tap/Assets/Blinker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blinker : MonoBehaviour
+{
+    [SerializeField] private BlinkColor[] colorBlinks;
+
+    [SerializeField] private AnimationCurve randomBlinkPeriod = AnimationCurve.Linear(0, .2f, 1, .2f);
+
+    private float nextBlinkElapced = 0;
+
+    private void Update()
+    {
+        if (nextBlinkElapced > 0)
+            nextBlinkElapced -= Time.deltaTime;
+        else
+            Blink();
+    }
+
+    private void Blink()
+    {
+        var t = Random.value;
+
+        foreach (var cb in colorBlinks)
+            cb?.Blink(t);
+
+        UpdateTimer();
+    }
+
+    private void UpdateTimer()
+    {
+        var t = Random.value;
+        nextBlinkElapced = randomBlinkPeriod.Evaluate(t);
+    }
+}
