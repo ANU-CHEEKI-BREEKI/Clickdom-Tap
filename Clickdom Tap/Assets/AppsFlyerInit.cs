a4062d3b211c4b34938f2bbe11c58b87b592aa54
ANU <titan.x7@gmail.com>
2020-01-07 11:36:22 +0200

    added AppsFlyer SDK and app init;

---
+++ Clickdom Tap/Assets/AppsFlyerInit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppsFlyerInit : MonoBehaviour
+{
+    private void Awake()
+    {
+        // Set AppsFlyer’s Developer key.
+        const string APPSFLYER_DEV_KEY = "zcKrZYJWnrWWctCxcLNnyT";
+        AppsFlyer.setAppsFlyerKey(APPSFLYER_DEV_KEY);
+        // For detailed logging
+        // AppsFlyer.setIsDebug (true);
+        // Set Android package name
+        AppsFlyer.setAppID(Application.identifier);
+        AppsFlyer.init(APPSFLYER_DEV_KEY);
+    }
+}
