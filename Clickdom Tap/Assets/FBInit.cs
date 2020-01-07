3f840fc693ec247991c872cc750a9966c64f0e2c
ANU <titan.x7@gmail.com>
2020-01-07 11:29:13 +0200

    added Facebook SDK and FB init App;

---
+++ Clickdom Tap/Assets/FBInit.cs
@@ -0,0 +1,40 @@
+﻿using Facebook.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FBInit : MonoBehaviour
+{
+    private void Awake()
+    {
+        if (!FB.IsInitialized)
+        {
+            FB.Init(
+                onInitComplete: () =>
+                {
+                    if (FB.IsInitialized)
+                    {
+                        FB.ActivateApp();
+                        print("FB app Activated");
+                    }
+                    else
+                    {
+                        print("FB app init failed");
+                    }
+                },
+                onHideUnity: (isGameShown) =>
+                {
+                    if (!isGameShown)
+                        PauseManager.Instance?.FullPauseGame();
+                    else
+                        PauseManager.Instance?.ResumeGame();
+                }
+            );
+        }
+        else
+        {
+            FB.ActivateApp();
+            print("FB app Activated");
+        }
+    }
+}
